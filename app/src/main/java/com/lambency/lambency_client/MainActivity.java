c9d249710e8761e1cc7f8899d3387e054b2dc96c
Farhan Shafi <fshafi@purdue.edu>
2018-02-06 18:42:00 -0500

    Test

--- app/src/main/java/com/lambency/lambency_client/MainActivity.java
+++ app/src/main/java/com/lambency/lambency_client/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Farhan test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
