bc00ec7a3fc61b5e584c11fbed9a93ed018b4791
Lucas Shanker <lshanke@purdue.edu>
2018-02-06 22:23:36 -0500

    Add some directories

--- app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lambency.lambency_client;
+package com.lambency.lambency_client.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lambency.lambency_client.R;
 
 public class MainActivity extends AppCompatActivity {
 
