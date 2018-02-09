e82e364d75dc5036eb1d537b0abc8e375ae232eb
Farhan Shafi <fshafi@purdue.edu>
2018-02-09 15:06:40 -0500

    Fixed merge conflict

--- app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
@@ -2,9 +2,20 @@ package com.lambency.lambency_client.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import com.lambency.lambency_client.R;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+//import com.lambency.lambency_client.R;
+
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -12,8 +23,14 @@ import com.facebook.FacebookException;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.lambency.lambency_client.R;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+//Key is 406595282653-cc9eb7143bvpgfe5da941r3jq174b4dq
+//this goes in src/main/resources/client_secret.json
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
