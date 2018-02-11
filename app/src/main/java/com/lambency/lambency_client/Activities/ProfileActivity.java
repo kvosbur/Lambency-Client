b86607bee8feee72f982d443a599f41724c381b0
Evan <ehoneyse@purdue.edu>
2018-02-11 12:44:37 -0500

    Added floating action button, working on making the page editable.

--- app/src/main/java/com/lambency/lambency_client/Activities/ProfileActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/ProfileActivity.java
@@ -1,16 +1,34 @@
 package com.lambency.lambency_client.Activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.lambency.lambency_client.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.myFAB)
+    FloatingActionButton myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.myFAB)
+    public void clickSend()
+    {
+
     }
 }
