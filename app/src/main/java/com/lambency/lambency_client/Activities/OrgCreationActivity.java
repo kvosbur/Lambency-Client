35b82cc7ede3ff151f0b8cee8bdce070839a83fd
Lucas Shanker <lshanke@purdue.edu>
2018-02-12 10:40:09 -0500

    Add rough layout for the organization creation page

---
+++ app/src/main/java/com/lambency/lambency_client/Activities/OrgCreationActivity.java
@@ -0,0 +1,23 @@
+package com.lambency.lambency_client.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.lambency.lambency_client.R;
+
+public class OrgCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_creation);
+
+        Spinner spinner = (Spinner) findViewById(R.id.stateSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.states, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
