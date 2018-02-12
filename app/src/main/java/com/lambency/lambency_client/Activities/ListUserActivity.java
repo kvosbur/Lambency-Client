3d4bf02deab536b2d10110d2b52c7c6c7ad877ca
Evan <ehoneyse@purdue.edu>
2018-02-12 16:10:11 -0500

    Have basic card and layout created.

--- app/src/main/java/com/lambency/lambency_client/Activities/ListUserActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/ListUserActivity.java
@@ -2,14 +2,37 @@ package com.lambency.lambency_client.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.lambency.lambency_client.R;
 
+import java.util.List;
+
 public class ListUserActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    List<String> content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_user);
+        mRecyclerView = (RecyclerView) findViewById(R.id.userRecycle);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new UserListAdapter(content);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
-}
