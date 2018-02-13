25f7b942011df599509c83491287e2a8f94f5f2b
Farhan Shafi <fshafi@purdue.edu>
2018-02-12 21:45:58 -0500

    After sign in go to main activity

--- app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.lambency.lambency_client.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.lambency.lambency_client.Models.UserModel;
 import com.lambency.lambency_client.R;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -32,12 +36,25 @@ public class MainActivity extends AppCompatActivity {
 //Key is 406595282653-cc9eb7143bvpgfe5da941r3jq174b4dq
 //this goes in src/main/resources/client_secret.json
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
 
+
+        //For Button
+        final Button button = findViewById(R.id.createEventButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // if statement for user will go here
+
+                // Start Eventcreationactivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        EventCreationActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/lambency/lambency_client/Models/UserModel.java
+++ app/src/main/java/com/lambency/lambency_client/Models/UserModel.java
@@ -1,8 +1,11 @@
 package com.lambency.lambency_client.Models;
 
+import java.util.List;
+
 /**
  * Created by lshan on 2/8/2018.
  */
 
 public class UserModel {
+
 }
