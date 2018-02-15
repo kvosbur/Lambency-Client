a18e0e4b4439578ff77457240954fc00cd81a51c
Farhan Shafi <fshafi@purdue.edu>
2018-02-14 20:57:57 -0500

    Users can now upload the event image

--- app/src/main/java/com/lambency/lambency_client/Activities/EventCreationActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/EventCreationActivity.java
@@ -1,6 +1,8 @@
 package com.lambency.lambency_client.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,18 +10,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lambency.lambency_client.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class EventCreationActivity extends AppCompatActivity {
     String eventName, dateOfEvent, addressOfEvent, description, contact;
+    ImageView eventImage;
+    private Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_creation);
+        this.context = this;
+        ButterKnife.bind(this);
 
         //Saving details when button pressed
         final Button saveDetails = findViewById(R.id.saveDetailsButton);
@@ -37,10 +53,55 @@ public class EventCreationActivity extends AppCompatActivity {
                 addressOfEvent = eAddr.getText().toString();
                 description = eDescrip.getText().toString();
                 contact = eContact.getText().toString();
-
             }
         });
     }
+
+    //Setting event image
+    @OnClick(R.id.eventImage)
+    public void setEventImage(){
+        EasyImage.openChooserWithGallery(this, "Select Event Image", 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+            }
+
+            @Override
+            public void onImagesPicked(List<File> imagesFiles, EasyImage.ImageSource source, int type) {
+                //Handle the images
+
+                Picasso.Builder builder = new Picasso.Builder(context);
+                builder.listener(new Picasso.Listener()
+                {
+                    @Override
+                    public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception)
+                    {
+                        exception.printStackTrace();
+                    }
+                });
+
+                builder.build()
+                        .load(new File(imagesFiles.get(0).getPath()))
+                        .error(R.drawable.ic_books)
+                        .into(eventImage);
+
+                /*
+                Bitmap bitmap = BitmapFactory.decodeFile(imagesFiles.get(0).getPath(), null);
+                profileImage.setImageBitmap(bitmap);
+                */
+            }
+        });
+    }
+
 
 
 }
