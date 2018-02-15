47da19858ce84f21c8dba36daa4b15ad1deb6faa
Lucas Shanker <lshanke@purdue.edu>
2018-02-15 16:21:39 -0500

    Connect search icon to search activity

--- app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/MainActivity.java
@@ -3,6 +3,9 @@ package com.lambency.lambency_client.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,18 +46,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //For Button
-        final Button button = findViewById(R.id.createEventButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // if statement for user will go here
-
-                // Start Eventcreationactivity.class
-                Intent myIntent = new Intent(MainActivity.this,
-                        EventCreationActivity.class);
-                startActivity(myIntent);
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_search){
+            Intent intent = new Intent(this, SearchActivity.class);
+            this.startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/lambency/lambency_client/Activities/SearchActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/SearchActivity.java
@@ -55,5 +55,14 @@ public class SearchActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 }
