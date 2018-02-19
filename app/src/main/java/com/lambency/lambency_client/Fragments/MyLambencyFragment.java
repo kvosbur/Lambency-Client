d63d1c46d86c392c6fd15424e330211981e4dd54
Evan Honeysett <ehoneyse@purdue.edu>
2018-02-19 18:17:10 -0500

    Main page now has button which take you to both org creation and event creation.

--- app/src/main/java/com/lambency/lambency_client/Fragments/MyLambencyFragment.java
+++ app/src/main/java/com/lambency/lambency_client/Fragments/MyLambencyFragment.java
@@ -1,40 +1,68 @@
 package com.lambency.lambency_client.Fragments;
 
-import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.lambency.lambency_client.Adapters.EventsMainAdapter;
-import com.lambency.lambency_client.Models.EventModel;
+import com.lambency.lambency_client.Activities.EventCreationActivity;
+import com.lambency.lambency_client.Activities.MainActivity;
+import com.lambency.lambency_client.Activities.OrgCreationActivity;
+import com.lambency.lambency_client.Activities.SearchActivity;
 import com.lambency.lambency_client.R;
 
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
- * Created by Evan on 2/19/2018.
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
+public class MyLambencyFragment extends Fragment {
 
-public class MyLambencyFragment extends Fragment
-{
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private EventsMainFragment.OnFragmentInteractionListener mListener;
-    private EventsMainAdapter eventsMainAdapter;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
+    @BindView(R.id.createOrgButton)
+    Button createOrg;
 
-    public MyLambencyFragment()
-    {
+    private OnFragmentInteractionListener mListener;
 
+    public MyLambencyFragment() {
+        // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static MyLambencyFragment newInstance(String param1, String param2) {
         MyLambencyFragment fragment = new MyLambencyFragment();
         Bundle args = new Bundle();
@@ -44,38 +72,89 @@ public class MyLambencyFragment extends Fragment
         return fragment;
     }
 
-
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof EventsMainFragment.OnFragmentInteractionListener) {
-            mListener = (EventsMainFragment.OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
 
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onEventFragmentInteraction(uri);
-        }
-    }
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
-        menuInflater.inflate(R.menu.menu_main, menu);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_lambency, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_lambency, container, false);
+        ButterKnife.bind(this, view);
+
+        Button createOrgButton = view.findViewById(R.id.createOrgButton);
+        createOrgButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OrgCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button createEventButton = view.findViewById(R.id.createEventButton);
+        createEventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), EventCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ((MainActivity) getActivity())
+                .setActionBarTitle("My Lambency");
+        ((MainActivity) getActivity()).getSupportActionBar().setElevation(0);
+
+        return view;
     }
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onProfileFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menu_edit, menu);
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onEventFragmentInteraction(Uri uri);
+        void onProfileFragmentInteraction(Uri uri);
     }
 }
