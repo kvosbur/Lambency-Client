abb791a181705da16f61c57460aaf6a05b61a26f
Evan <ehoneyse@purdue.edu>
2018-02-22 18:42:41 -0500

    Fixed issue with wrong type

--- app/src/main/java/com/lambency/lambency_client/Activities/OrganizationDetailsActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/OrganizationDetailsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,7 +49,7 @@ public class OrganizationDetailsActivity extends AppCompatActivity {
     CircleImageView orgImage;
 
     @BindView(R.id.orgRequestJoin)
-    CircleImageView requestJoin;
+    Button requestJoin;
 
 
     @Override
