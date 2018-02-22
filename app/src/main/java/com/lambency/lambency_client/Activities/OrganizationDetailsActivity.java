ce6753f45b6d3a8d267ca1b3ecac69da86910770
Farhan Shafi <fshafi@purdue.edu>
2018-02-22 16:12:43 -0500

    checkbox for follow un follow functionality added...

--- app/src/main/java/com/lambency/lambency_client/Activities/OrganizationDetailsActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/OrganizationDetailsActivity.java
@@ -5,19 +5,44 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.lambency.lambency_client.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class OrganizationDetailsActivity extends AppCompatActivity {
+
+@BindView(R.id.followUnFollow)
+CheckBox checkBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_details);
 
+        ButterKnife.bind(this);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()){
+                    Toast.makeText(getApplicationContext(), "You are now following the organization", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "You un followed the organization", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
     @Override
