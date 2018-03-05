745759fc03325a63270d3b7ddd77e831acd45e72
Kevin Vosburgh <kvosbur@purdue.edu>
2018-03-05 12:25:44 -0500

    added mylambency call to retrofit interface.

--- app/src/main/java/com/lambency/lambency_client/Networking/LambencyAPI.java
+++ app/src/main/java/com/lambency/lambency_client/Networking/LambencyAPI.java
@@ -1,6 +1,7 @@
 package com.lambency.lambency_client.Networking;
 
 import com.lambency.lambency_client.Models.EventModel;
+import com.lambency.lambency_client.Models.MyLambencyModel;
 import com.lambency.lambency_client.Models.OrganizationModel;
 import com.lambency.lambency_client.Models.UserAuthenticatorModel;
 import com.lambency.lambency_client.Models.UserModel;
@@ -77,6 +78,9 @@ public interface LambencyAPI {
 
     @GET("Organization/events")
     Call<List<EventModel>> getEventsByOrg(@Query("oAuthCode") String oAuthCode, @Query("id") String org_id);
+
+    @GET("User/MyLambency")
+    Call<MyLambencyModel> getMyLambencyModel(@Query("oAuthCode") String oAuthCode);
 
 
 }
