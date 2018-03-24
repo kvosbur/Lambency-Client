a2bcf7f6470be4f3aa64786d509c31b4da1e3659
Evan <ehoneyse@purdue.edu>
2018-03-24 15:47:45 -0400

    Now shows correct orgs

--- app/src/main/java/com/lambency/lambency_client/Activities/EventDetailsActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/EventDetailsActivity.java
@@ -20,6 +20,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -485,8 +486,7 @@ public class EventDetailsActivity extends AppCompatActivity implements
                     // TODO fill here
                     listOfEndorseOrg = new OrganizationAdapter(context, orgList);
                     orgEndorseList.setAdapter(listOfEndorseOrg);
-                    Toast.makeText(getApplicationContext(), orgList.size(), Toast.LENGTH_LONG).show();
-
+                    orgEndorseList.setLayoutManager(new LinearLayoutManager(context));
                 }
             }
 
