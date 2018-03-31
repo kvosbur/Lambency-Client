a13a75a2994e6a8499edf4474c16d1496ee6992a
Evan <ehoneyse@purdue.edu>
2018-03-31 16:10:26 -0400

    Adapter and layouts made

---
+++ app/src/main/java/com/lambency/lambency_client/Utils/Message.java
@@ -0,0 +1,32 @@
+package com.lambency.lambency_client.Utils;
+
+/**
+ * Created by Evan on 3/31/2018.
+ */
+
+public class Message {
+
+    public String messageText;
+    public String sender;
+    public long createdAt;
+
+    public Message(){
+
+    }
+
+    public Message(String messageText, String sender)
+    {
+        this.messageText = messageText;
+        this.sender = sender;
+    }
+
+    public String getMessage()
+    {
+        return messageText;
+    }
+
+    public String getSender()
+    {
+        return sender;
+    }
+}
