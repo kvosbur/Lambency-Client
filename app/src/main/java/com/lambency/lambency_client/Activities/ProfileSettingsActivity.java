260a0db27ab49c63783b20f9e61a3b604056a837
Farhan Shafi <fshafi@purdue.edu>
2018-04-09 15:21:08 -0400

    added a settings preference with many features

--- app/src/main/java/com/lambency/lambency_client/Activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/ProfileSettingsActivity.java
@@ -3,6 +3,8 @@ package com.lambency.lambency_client.Activities;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
@@ -17,18 +19,40 @@ import com.lambency.lambency_client.Networking.LambencyAPI;
 import com.lambency.lambency_client.Networking.LambencyAPIHelper;
 import com.lambency.lambency_client.R;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ProfileSettingsActivity extends AppCompatActivity {
+public class ProfileSettingsActivity extends AppCompatPreferenceActivity {
+    private static final String TAG = ProfileSettingsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_settings);
+        //setContentView(R.layout.activity_profile_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //the change password button
+        // load settings fragment
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
+
+        /*//the change password button
         final Button changePassword = findViewById(R.id.changePasswordBtn);
         changePassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -142,77 +166,126 @@ public class ProfileSettingsActivity extends AppCompatActivity {
 
                 dialog.show();
 
-
-                /*alertDialog.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                boolean check = true;
-
-
-                                if (!newPass.getText().toString().equals(confirmPass.getText().toString())){
-                                    check = false;
-                                    Toast.makeText(ProfileSettingsActivity.this, "The two passwords don't match",
-                                            Toast.LENGTH_LONG).show();
-                                }
-                                else if (newPass.getText().toString().matches("")){
-                                    check = false;
-                                    newPass.setError("Please enter a password");
-                                    Toast.makeText(ProfileSettingsActivity.this, "Password entry field one empty",
-                                            Toast.LENGTH_LONG).show();
-                                }
-                                else if (confirmPass.getText().toString().matches("")){
-                                    check = false;
-                                    confirmPass.setError("Please enter same password");
-                                    Toast.makeText(ProfileSettingsActivity.this, "Password entry field two empty",
-                                            Toast.LENGTH_LONG).show();
-
-                                }
-                                if (check == true) {
-                                    LambencyAPIHelper.getInstance().changePassword(newPass.getText().toString(),
-                                            confirmPass.getText().toString(), UserModel.myUserModel.getOauthToken(),oldPass.getText().toString())
-                                            .enqueue(new Callback<Integer>() {
-                                                @Override
-                                                public void onResponse(Call<Integer> call, Response<Integer> response) {
-                                                    if (response.body() == null || response.code() != 200) {
-                                                        Toast.makeText(getApplicationContext(), "Server error!", Toast.LENGTH_SHORT).show();
-                                                        return;
-                                                    }
-                                                    int ret = response.body();
-
-                                                    if (ret == 0){
-                                                        Toast.makeText(getApplicationContext(), "Password change success", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                    else if (ret == 7){
-                                                        Toast.makeText(getApplicationContext(), "You entered the old password wrong Pls try again", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                    else {
-                                                        Toast.makeText(getApplicationContext(), "General Server error!", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                }
-
-                                                @Override
-                                                public void onFailure(Call<Integer> call, Throwable t) {
-                                                    System.out.println("Failed in change password");
-                                                }
-                                            });
-                                    dialog.cancel();
-                                }
-                            }
-                        });
-
-
-                alertDialog.setNegativeButton("No",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-                AlertDialog alert11 = alertDialog.create();
-                alert11.show();*/
-
             }
 
-        });
-
+        });*/
     }
+
+    public static class MainPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_main);
+
+            // gallery EditText change listener
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_gallery_name)));
+
+            // notification preference change listener
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_notifications_new_message_ringtone)));
+
+            // feedback preference click listener
+            Preference myPref = findPreference(getString(R.string.key_send_feedback));
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    sendFeedback(getActivity());
+                    return true;
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    /**
+     * A preference value change listener that updates the preference's summary
+     * to reflect its new value.
+     */
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+
+            } else if (preference instanceof RingtonePreference) {
+                // For ringtone preferences, look up the correct display value
+                // using RingtoneManager.
+                if (TextUtils.isEmpty(stringValue)) {
+                    // Empty values correspond to 'silent' (no ringtone).
+                    preference.setSummary(R.string.pref_ringtone_silent);
+
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(
+                            preference.getContext(), Uri.parse(stringValue));
+
+                    if (ringtone == null) {
+                        // Clear the summary if there was a lookup error.
+                        preference.setSummary(R.string.summary_choose_ringtone);
+                    } else {
+                        // Set the summary to reflect the new ringtone display
+                        // name.
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
+
+            } else if (preference instanceof EditTextPreference) {
+                if (preference.getKey().equals("change_Password_Preference")) {
+                    //set what to do here
+                    //preference.setSummary(stringValue);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    /**
+     * Email client intent to send support mail
+     * Appends the necessary device information to email body
+     * useful when providing support
+     */
+    public static void sendFeedback(Context context) {
+        String body = null;
+        try {
+            body = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            body = "\n\n-----------------------------\nPlease don't remove this information\n Device OS: Android \n Device OS version: " +
+                    Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
+                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"contact@androidhive.info"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Query from android app");
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        context.startActivity(Intent.createChooser(intent, context.getString(R.string.choose_email_client)));
+    }
+
 }
