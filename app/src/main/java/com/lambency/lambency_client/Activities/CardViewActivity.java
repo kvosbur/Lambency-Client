85326d9847b9cf35f201589177e0355b3336ab08
Farhan Shafi <fshafi@purdue.edu>
2018-04-15 17:20:34 -0400

    checking pasthours

--- app/src/main/java/com/lambency/lambency_client/Activities/CardViewActivity.java
+++ app/src/main/java/com/lambency/lambency_client/Activities/CardViewActivity.java
@@ -12,11 +12,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lambency.lambency_client.Fragments.ProfileFragment;
+import com.lambency.lambency_client.Models.EventModel;
+import com.lambency.lambency_client.Models.UserAuthenticatorModel;
+import com.lambency.lambency_client.Models.UserModel;
+import com.lambency.lambency_client.Networking.LambencyAPI;
+import com.lambency.lambency_client.Networking.LambencyAPIHelper;
 import com.lambency.lambency_client.R;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CardViewActivity extends AppCompatActivity {
 
@@ -24,6 +36,11 @@ public class CardViewActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static String LOG_TAG = "CardViewActivity";
+    private ArrayList<EventModel> eventModels = new ArrayList<EventModel>();
+    private ArrayList<Double> hoursOfEvents = new ArrayList<Double>();
+    private boolean eventCheck = true;
+    int userIdKey;
+    String orgIdKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +52,80 @@ public class CardViewActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        Intent myIntent = getIntent();
+        userIdKey = myIntent.getIntExtra("userIdKey", 0);
+        orgIdKey = myIntent.getStringExtra("orgIdKey");
+
+
         //TODO: call retrofit and give correct things to getdataset some way to differentiate to retrofits
-        mAdapter = new MyRecyclerViewAdapter(getDataSet());
-        mRecyclerView.setAdapter(mAdapter);
+
+        if (orgIdKey != null) {
+            LambencyAPIHelper.getInstance().getPastEventsInOrg(UserAuthenticatorModel.myAuth, userIdKey + "", orgIdKey).enqueue(new Callback<ArrayList<EventModel>>() {
+                @Override
+                public void onResponse(Call<ArrayList<EventModel>> call, Response<ArrayList<EventModel>> response) {
+                    if (response.body() == null || response.code() != 200) {
+                        System.out.println("An error has occurred or the user has no past events or user is not admin of org");
+                        // if null is given show that the user has no past events
+                        eventCheck = false;
+                        return;
+                    }
+                    //when response is back
+                    ArrayList<EventModel> events = response.body();
+                    for (EventModel eventModel : events) {
+                        // i set hours in description
+                        eventModels.add(eventModel);
+                        double hours = Double.parseDouble(eventModel.getDescription());
+                        hoursOfEvents.add(hours);
+                        System.out.println("User worked " + hours + " hours at " + eventModel.getName());
+                    }
+
+                    mAdapter = new MyRecyclerViewAdapter(getDataSet());
+                    mRecyclerView.setAdapter(mAdapter);
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<EventModel>> call, Throwable t) {
+                    //when failure
+                    System.out.println("FAILED CALL");
+                }
+            });
+        } else {
+            System.out.println(" in the profile one    !!!!!");
+            LambencyAPIHelper.getInstance().getPastEvents(UserAuthenticatorModel.myAuth).enqueue(new Callback<ArrayList<EventModel>>() {
+                @Override
+                public void onResponse(Call<ArrayList<EventModel>> call, Response<ArrayList<EventModel>> response) {
+                    if (response.body() == null || response.code() != 200) {
+                        System.out.println("An error has occurred or the user has no past events");
+                        // if null is given show that the user has no past events
+                        eventCheck = false;
+                        return;
+                    }
+                    //when response is back
+                    ArrayList<EventModel> events = response.body();
+                    for (EventModel eventModel : events) {
+                        // i set hours in description
+                        eventModels.add(eventModel);
+                        double hours = Double.parseDouble(eventModel.getDescription());
+                        hoursOfEvents.add(hours);
+                        System.out.println("User worked " + hours + " hours at " + eventModel.getName());
+                    }
+
+                    mAdapter = new MyRecyclerViewAdapter(getDataSet());
+                    mRecyclerView.setAdapter(mAdapter);
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<EventModel>> call, Throwable t) {
+                    //when failure
+                    System.out.println("FAILED CALL");
+                }
+            });
+        }
 
     }
 
 
-    @Override
+    /*@Override
     protected void onResume() {
         super.onResume();
         ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
@@ -52,15 +135,24 @@ public class CardViewActivity extends AppCompatActivity {
                 Log.i(LOG_TAG, " Clicked on Item " + position);
             }
         });
-    }
+    }*/
 
     private ArrayList<DataObject> getDataSet() {
-        ArrayList results = new ArrayList<DataObject>();
-        for (int index = 0; index < 20; index++) {
-            DataObject obj = new DataObject("Name of event will go here " + index,
-                    "hours attended go here " + index);
-            results.add(index, obj);
+        if (eventCheck == true && eventModels != null) {
+            ArrayList results = new ArrayList<DataObject>();
+            for (int index = 0; index < eventModels.size(); index++) {
+                DataObject obj = new DataObject(eventModels.get(index).getName(),
+                        "hours attended: " + hoursOfEvents.get(index));
+                results.add(index, obj);
+            }
+            return results;
+        } else {
+            ArrayList results = new ArrayList<DataObject>();
+            TextView topView = findViewById(R.id.topTextView);
+            topView.setText("You attended 0 events");
+            DataObject obj = new DataObject("You have not attended any events", "No hours to show");
+            results.add(obj);
+            return results;
         }
-        return results;
     }
 }
